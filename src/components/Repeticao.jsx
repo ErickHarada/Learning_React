ddab072d0df6bda2ce1781c0823f82604730bc80
Erick <eefeaga2000@gmail.com>
2021-07-08 16:37:05 -0300

    Aprendendo React - Aula 01

---
+++ src/components/Repeticao.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Produtos from '../data/Produtos';
+
+export default props => {
+
+    function getProdutos() {
+        return Produtos.map(p => {
+            return <li key={p.id}>
+                {p.id} - {p.name} - R$ {p.price}
+            </li>
+        })
+    }
+
+    return (
+        <div>
+            <h2>Repetição</h2>
+            <ul>
+                {getProdutos()}
+            </ul>
+        </div>
+    )
+}
